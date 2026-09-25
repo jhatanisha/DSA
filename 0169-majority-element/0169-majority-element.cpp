71985e10916e29572eb68a811c92f691e70c3280
Tanisha katyayan <jhatanisha1331@gmail.com>
2026-09-26 01:54:15 +0530

    Time: 0 ms (100%), Space: 42.3 MB (7.89%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        map<int,int> mp;
+        int i=0;
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+            if(mp[nums[i]]>n/2){
+            return nums[i];
+            }
+        }
+        return -1;
+    }
+};
